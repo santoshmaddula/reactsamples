c46a3bda5939f9930bd2e76adb8193409dd1d33e
santoshmaddula <sk.maddula@outlook.com>
2021-02-09 17:17:16 +0530

    1 file modified

--- src/component/Greetings.js
+++ src/component/Greetings.js
@@ -8,10 +8,13 @@ function Greetings() {
 
   if (hrs < 12) {
     timeOfDay = "Good Morning";
+    console.log(timeOfDay);
   } else if (12 < hrs < 20) {
     timeOfDay = "Good Evening";
+    console.log(timeOfDay);
   } else {
     timeOfDay = "Good Night";
+    console.log(timeOfDay);
   }
 
   return <p>{timeOfDay}</p>;
